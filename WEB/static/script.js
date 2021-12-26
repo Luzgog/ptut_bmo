85232b778d8e21381099ad5889f001437f70afca
NinoZore <60515907+NinoZore@users.noreply.github.com>
2021-12-27 00:12:01 +0100

    Add files via upload

---
+++ WEB/static/script.js
@@ -0,0 +1,52 @@
+function temperature(){
+    
+    fetch("/temperature")//on fait une requete a /message
+    .then( data => {console.log(data);return data.json()})//on prend ce que ca nous renvoie et on retourne le json
+    .then( j =>{//avec le json on va modifier la valeur pour la changer
+        console.log(j);
+        var valeur = document.getElementById("TEMP")//varriable html type : id
+
+        valeur.innerText = j["TEMPE"]//varriable python
+    })
+}
+
+function batterie(){
+    
+    fetch("/batterie")//on fait une requete a /message
+    .then( data => {console.log(data);return data.json()})//on prend ce que ca nous renvoie et on retourne le json
+    .then( j =>{//avec le json on va modifier la valeur pour la changer
+        console.log(j);
+        var valeur = document.getElementById("BATT")
+
+        valeur.innerText = j["BATTE"]
+    })
+}
+
+function etats(){
+    
+    fetch("/etats")//on fait une requete a /message
+    .then( data => {console.log(data);return data.json()})//on prend ce que ca nous renvoie et on retourne le json
+    .then( j =>{//avec le json on va modifier la valeur pour la changer
+        console.log(j);
+        var valeur = document.getElementById("STAT")
+
+        valeur.innerText = j["STATS"]
+    })
+}
+
+function humeurs(){
+    
+    fetch("/humeurs")//on fait une requete a /message
+    .then( data => {console.log(data);return data.json()})//on prend ce que ca nous renvoie et on retourne le json
+    .then( j =>{//avec le json on va modifier la valeur pour la changer
+        console.log(j);
+        var valeur = document.getElementById("MOOD")
+
+        valeur.innerText = j["HUMER"]
+    })
+}
+
+setInterval(humeurs, 1000)
+setInterval(etats, 1000)
+setInterval(batterie, 1000)
+setInterval(temperature, 1000)
