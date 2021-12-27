69585930281cd5588ae95005162805493de1b878
TABARDEL <bastientabardel@outlook.com>
2021-12-27 14:44:27 +0100

    correction script lolcatisé

--- Programmes/Script.sh
+++ Programmes/Script.sh
@@ -7,6 +7,18 @@ YELLOW='\033[1;33m'
 BLUE='\033[1;34m'
 NC='\033[0m'
 
+if [[ "$EUID" = 0 ]]; then
+    echo "Vous etes root"
+else
+    sudo -k # make sure to ask for password on next sudo
+    if sudo true; then
+        echo "Mot de passe correct"
+    else
+        echo "Mot de passe incorrect"
+        exit 1
+    fi
+fi
+
 if (whiptail --title "Installation" --yesno "voulez vous lancer l'installation ou la mise à jour de BMO ?" --yes-button "Installation" --no-button "Mise à Jour" 10 60) then  
     printf "%b\n" "   ${GREEN}////////////////////////////////////////////////\n   ${YELLOW}//      Début du programme d'installation      //\n   ${RED}////////////////////////////////////////////////${NC}\n"    
     echo ""
@@ -15,46 +27,39 @@ if (whiptail --title "Installation" --yesno "voulez vous lancer l'installation o
 
     printf "%b\n" "${BLUE}     ********************************\n     *   Mise a jour du raspberry   *\n     ********************************${NC}\n"
     echo ""
-    sudo apt-get install -y 
-
-    sudo apt-get update -y 
-    sudo apt-get dist-upgrade -y 
-    sudo rpi-update -y 
-    sudo apt-get upgrade -y 
-    sudo apt update -y 
+    sudo apt-get install lolcat -y 
+    sudo apt-get update -y |lolcat
+    sudo apt-get dist-upgrade -y |lolcat
+    sudo rpi-update -y |lolcat
+    sudo apt-get upgrade -y |lolcat
+    sudo apt update -y |lolcat
     echo ""
 
 
 
     printf "%b\n" "${BLUE}     *************************************************\n     *   installation des librarys  *\n     *************************************************${NC}\n"
     echo ""
-    sudo apt install pip libopencv-dev python3-opencv git -y 
-    sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev -y 
-    sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev libatlas-base-dev i2c-tools -y 
-    sudo pip3 install dlib cvlib 
-    sudo pip3 install face_recognition 
-    sudo pip3 install smbus2 
-    sudo pip3 install Flask 
-
+    sudo apt install pip libopencv-dev python3-opencv git cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev libatlas-base-dev i2c-tools -y 
+    sudo pip3 install dlib cvlib face_recognition smbus2 Flask
     echo ""
 
     printf "%b\n" "${BLUE}     *************************************************\n     *   Mise en place de l'interface WEB  *\n     *************************************************${NC}\n"
     
-    sudo git clone https://github.com/Luzgog/ptut_bmo.git
+    sudo git clone https://github.com/Luzgog/ptut_bmo.git |lolcat
     echo ""
-    sudo mkdir BMO
-    sudo mv /home/pi/ptut_bmo/WEB/static /home/pi/BMO
-    sudo mv /home/pi/ptut_bmo/Programmes/cerveau.py /home/pi/BMO
+    sudo mkdir BMO 
+    sudo mv /home/pi/ptut_bmo/WEB/static /home/pi/BMO 
+    sudo mv /home/pi/ptut_bmo/Programmes/cerveau.py /home/pi/BMO 
 
 
 
 else
     whiptail --title "Mise à Jour" --msgbox "Mise a jour en cours" 10 60
 
-    sudo rm -r /home/pi/BMO
+    sudo rm -r /home/pi/BMO 
     sudo rm -r /home/pi/ptut_bmo
     echo ""
-    sudo cd
+    cd
     sudo git clone https://github.com/Luzgog/ptut_bmo.git
     echo ""
     sudo mkdir /home/pi/BMO
@@ -73,5 +78,5 @@ else
 fi
 
 echo "n'oublier pas d'activer la camera et le bus i2c via l'interface raspi-config"
-echo "Script par Nino Nicolas"
+echo "Script par Nino Nicolas avec l'aide de Bastien Tabardel"
 exit
