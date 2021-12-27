e5f3d5a11d2f163275a0e3c6cf15583e887836f2
NinoZore <60515907+NinoZore@users.noreply.github.com>
2021-12-27 14:56:20 +0100

    Update Script.sh

--- Programmes/Script.sh
+++ Programmes/Script.sh
@@ -39,8 +39,8 @@ if (whiptail --title "Installation" --yesno "voulez vous lancer l'installation o
 
     printf "%b\n" "${BLUE}     *************************************************\n     *   installation des librarys  *\n     *************************************************${NC}\n"
     echo ""
-    sudo apt install pip libopencv-dev python3-opencv git cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev libatlas-base-dev i2c-tools -y 
-    sudo pip3 install dlib cvlib face_recognition smbus2 Flask
+    sudo apt-get install pip libopencv-dev python3-opencv git cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev libatlas-base-dev i2c-tools -y |lolcat 
+    sudo pip3 install dlib cvlib face_recognition smbus2 Flask |lolcat
     echo ""
 
     printf "%b\n" "${BLUE}     *************************************************\n     *   Mise en place de l'interface WEB  *\n     *************************************************${NC}\n"
