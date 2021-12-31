d081b8c900fbfd0ac359787691b8e54cd9645f11
NinoZore <60515907+NinoZore@users.noreply.github.com>
2021-12-31 01:27:45 +0100

    Update son.py

--- son/son.py
+++ son/son.py
@@ -1,6 +1,7 @@
 import pygame
 volume = 100
-
+#le volume doit être en 0 et 1 plus d'info ici :
+# https://www.pygame.org/docs/ref/music.html#pygame.mixer.music.set_volume
 pygame.mixer.init()
 pygame.mixer.music.load("myFile.wav")
 pygame.mixer.music.set_volume(volume / 100)
