12c39a4f44940d5f9a677a674f92ba9e19036452
NinoZore <60515907+NinoZore@users.noreply.github.com>
2021-12-31 01:08:18 +0100

    Update son.py

--- son/son.py
+++ son/son.py
@@ -1,8 +1,10 @@
 import pygame
+volume = 100
 
 pygame.mixer.init()
 pygame.mixer.music.load("myFile.wav")
-pygame.mixer.music.set_volume(0.1)
+pygame.mixer.music.set_volume(volume / 100)
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy() == True:
     continue
+    
