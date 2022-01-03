6315221301243f5a6b5a2f6826b91a13ec3609bc
NinoZore <60515907+NinoZore@users.noreply.github.com>
2022-01-03 21:54:52 +0100

    Update oled.py

--- affichage_oled/oled.py
+++ affichage_oled/oled.py
@@ -2,9 +2,9 @@ from luma.core.interface.serial import i2c
 from luma.oled.device import sh1106
 from PIL import Image
 
-serial = i2c(port=1, address=0x3C)
+ecranbus = i2c(port=1, address=0x3C)
 
-device = sh1106(serial)
+device = sh1106(ecranbus)
 device.clear() #on enleve l'image deja existant si il y en a
 #tu as aussi hide  qui va eteindre ou mettre en low wonsommation la carte
 img = Image.open("Oeil_2.png")
