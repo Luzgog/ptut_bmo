0561825bd7ece0f10bba3bf162b0c29d3a1564b7
NinoZore <60515907+NinoZore@users.noreply.github.com>
2022-01-03 21:58:09 +0100

    Update oled.py

--- affichage_oled/oled.py
+++ affichage_oled/oled.py
@@ -4,10 +4,10 @@ from PIL import Image
 
 ecranbus = i2c(port=1, address=0x3C)
 
-device = sh1106(ecranbus)
-device.clear() #on enleve l'image deja existant si il y en a
+ecranoled = sh1106(ecranbus)
+ecranoled.clear() #on enleve l'image deja existant si il y en a
 #tu as aussi hide  qui va eteindre ou mettre en low wonsommation la carte
 img = Image.open("Oeil_2.png")
 
-device.display(img.convert(device.mode))
+ecranoled.display(img.convert(ecranoled.mode))
 input() #j'ai mit ca juste pour attendre, a la fin du programme ca enleve l'image automatiquement
