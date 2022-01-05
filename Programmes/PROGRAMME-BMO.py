ea71ff600678f85459ed498a1005610fbacbbcdb
NinoZore <ncls.nino13@gmail.com>
2022-01-05 22:07:13 +0100

    fzz

--- Programmes/PROGRAMME-BMO.py
+++ Programmes/PROGRAMME-BMO.py
@@ -299,14 +299,14 @@ def humeureu():
 def meteo_api():
     global temperature
     global meteo
-        
+    print("TRUC")        
     if Activer_Meteo == True:
         r_weather = requests.get(url_weather)
         data = r_weather.json()
         temperature = data['main']['temp'] # .str(temperature)
         meteo = data['weather'][0]['description'] # .str(meteo)
         print("machin TRUE")    
-    else:
+    else :
         temperature = "NON ACTIVER"
         meteo = "NON ACTIVER"
         print("machin FALSE")    
