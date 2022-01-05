c4404acc8c4cfce89f02a5d0dbdc236807e79c96
TABARDEL <bastientabardel@outlook.com>
2022-01-05 21:35:03 +0100

    ff

--- Programmes/Script.sh
+++ Programmes/Script.sh
@@ -59,8 +59,12 @@ if (whiptail --title "Installation" --yesno "voulez vous lancer l'installation o
 
 else
     whiptail --title "Mise à Jour" --msgbox "Mise a jour en cours" 10 60
-
     git pull
+    sudo mkdir /home/pi/BMO
+    sudo mv /home/pi/ptut_bmo/WEB/static /home/pi/BMO
+    sudo mv /home/pi/ptut_bmo/affichage_oled /home/pi/BMO
+    sudo mv /home/pi/ptut_bmo/Programmes/PROGRAMME-BMO.py /home/pi/BMO
+    sudo mv /home/pi/ptut_bmo/son/boot.wav /home/pi/BMO
 
 fi
 
