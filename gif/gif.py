df3720c43e09d1753c982b63d217da7bfbe02685
NinoZore <ncls.nino13@gmail.com>
2022-01-08 18:14:51 +0100

    dzfz

--- gif/gif.py
+++ gif/gif.py
@@ -10,7 +10,7 @@ ecranD = ST7789.ST7789(
         cs=ST7789.BG_SPI_CS_FRONT,  # BG_SPI_CS_BACK or BG_SPI_CS_FRONT
         dc=9,
         backlight=19,               # 18 for back BG slot, 19 for front BG slot.
-        spi_speed_hz=100 * 1000 * 1000,
+        spi_speed_hz=80 * 1000 * 1000,
         offset_left= 40,
         offset_top= 0
 )
@@ -21,7 +21,7 @@ ecranG = ST7789.ST7789(
         cs=ST7789.BG_SPI_CS_BACK,  # BG_SPI_CS_BACK or BG_SPI_CS_FRONT
         dc=9,
         backlight=19,               # 18 for back BG slot, 19 for front BG slot.
-        spi_speed_hz=100 * 1000 * 1000,
+        spi_speed_hz=80 * 1000 * 1000,
         offset_left= 40,
         offset_top= 0
 )
