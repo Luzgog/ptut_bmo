b923ae868d8af7771ae4190bac97c98691922337
NinoZore <ncls.nino13@gmail.com>
2022-01-08 13:23:36 +0100

    ecran gauche et droite

--- gif/gif.py
+++ gif/gif.py
@@ -3,7 +3,7 @@ from PIL import Image
 import ST7789
 import time
 
-disp = ST7789.ST7789(
+ecranD = ST7789.ST7789(
         height= 240,
         rotation= 0,
         port=0,
@@ -14,23 +14,37 @@ disp = ST7789.ST7789(
         offset_left= 40,
         offset_top= 0
 )
+ecranG = ST7789.ST7789(
+        height= 240,
+        rotation= 180,
+        port=1,
+        cs=ST7789.BG_SPI_CS_FRONT,  # BG_SPI_CS_BACK or BG_SPI_CS_FRONT
+        dc=9,
+        backlight=19,               # 18 for back BG slot, 19 for front BG slot.
+        spi_speed_hz=80 * 1000 * 1000,
+        offset_left= 40,
+        offset_top= 0
+)
     # Initialize display.
-disp.begin()
 
-width = disp.width
-height = disp.height
+ecranD.begin()
+ecranG.begin()
+
+width = ecranD.width
+height = ecranD.height
 
 # Load an image.
-image = Image.open(shutdown2.gif)
+imageD = Image.open("shutdown2.gif")
+imageG = Image.open("shutdown.gif")
 
 frame = 0
 
 while True:
     try:
         image.seek(frame)
-        disp.display(image.resize((width, height)))
+        ecranD.display(image.resize((width, height)))
         frame += 1
-        time.sleep(0.05)
+        time.sleep(0.03)
 
     except EOFError:
         frame = 0
