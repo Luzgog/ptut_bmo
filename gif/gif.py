fb867f6aa7aa05db7ddcc5cf2275136850eaff2c
NinoZore <ncls.nino13@gmail.com>
2022-01-08 12:39:09 +0100

    dz

--- gif/gif.py
+++ gif/gif.py
@@ -4,15 +4,15 @@ import ST7789
 import time
 
 disp = ST7789.ST7789(
-        height=135 if display_type == "rect" else 240,
-        rotation=0 if display_type == "rect" else 90,
+        height= 240,
+        rotation= 0,
         port=0,
         cs=ST7789.BG_SPI_CS_FRONT,  # BG_SPI_CS_BACK or BG_SPI_CS_FRONT
         dc=9,
         backlight=19,               # 18 for back BG slot, 19 for front BG slot.
         spi_speed_hz=80 * 1000 * 1000,
-        offset_left=0 if display_type == "square" else 40,
-        offset_top=53 if display_type == "rect" else 0
+        offset_left= 40,
+        offset_top= 0
 )
     # Initialize display.
 disp.begin()
@@ -21,10 +21,7 @@ width = disp.width
 height = disp.height
 
 # Load an image.
-print('Loading gif: {}...'.format(image_file))
-image = Image.open(image_file)
-
-print('Drawing gif, press Ctrl+C to exit!')
+image = Image.open(shutdown2.gif)
 
 frame = 0
 
