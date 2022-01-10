dd3c99ca9e2e4a5a95e551c5165ae97fdd0d8d7e
NinoZore <ncls.nino13@gmail.com>
2022-01-10 03:00:37 +0100

    uttbg

--- vocal/vocal.ino
+++ vocal/vocal.ino
@@ -3,6 +3,8 @@
 
 VR myVR(2,3);
 uint8_t buf[64];
+int ret;
+bool menu;
 
 #define onRecord    (0)
 #define offRecord   (1)
@@ -22,30 +24,54 @@ void setup() {
     if(myVR.load((uint8_t)onRecord) >= 0){
       Serial.println("onRecord loaded");
     }
-  
     if(myVR.load((uint8_t)offRecord) >= 0){
       Serial.println("offRecord loaded");
     }
+
 }
 
 void loop() {
-  int ret;
+  menu = HIGH;
   ret = myVR.recognize(buf, 50);
   if(ret>0){
 
     myVR.clear();
-    myVR.load((uint8_t)1);
-    myVR.load((uint8_t)6);
-    myVR.load((uint8_t)69);
+    myVR.load((uint8_t)1);//hey BMO
 
     if(buf[1] == 1){
-        Serial.println("hey BMO");    
-    }
-    if(buf[1] == 6){
-        Serial.println("SEIGNEUR SEVEN");    
-    }
-    if(buf[1] == 69){
-        Serial.println("COCHON VA !");    
-    }
-  }
+        buf[1] = 0;
+        delay(500);
+        Serial.println("MENU");
+        
+        myVR.clear();
+        myVR.load((uint8_t)5);//shutdown
+        myVR.load((uint8_t)6);//test
+        myVR.load((uint8_t)7);
+        myVR.load((uint8_t)8);
+        myVR.load((uint8_t)9);//mode
+        myVR.load((uint8_t)10);//paramètre
+        
+        while(menu == HIGH){
+          myVR.recognize(buf, 50);
+          
+          if(buf[1] == 5){
+              Serial.println("Shutdown");
+              menu = LOW;
+          }
+          if(buf[1] == 6){
+              Serial.println("Simon");
+              menu = LOW;
+          }
+          if(buf[1] == 9){
+              Serial.println("Mode");
+              menu = LOW;
+          }        
+          if(buf[1] == 10){
+              Serial.println("Paramètre");
+              menu = LOW;
+          }
+
+        }
+      }
+   }
 }
