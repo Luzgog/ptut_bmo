a34d4e43cef23ea0ff5ad741560f1e21c1fb4e5d
Bastien Tabardel <bastientabardel@outlook.com>
2022-01-19 16:05:35 +0000

    seance ptut

--- facial_programme/option1.py
+++ facial_programme/option1.py
@@ -20,10 +20,10 @@ def encodage_visage():
 def display(img):
     cv2.imshow(img)
     cv2.waitKey(1)
-def traitement_image(img):
-    crop = img[100:480 , 200:550]
-    small_frame = cv2.resize(crop, (0, 0), fx=0.25, fy=0.25)#division de la taille par 4 pour gagner en rapidité (mais perte precision)
-    gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)#on convertie en noir et blanc
+def traitement_image(img):  
+    #crop = img[100:480 , 200:550]
+    small_frame = cv2.resize(img, (0, 0), fx=0.25, fy=0.25)#division de la taille par 4 pour gagner en rapidité (mais perte precision)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)#on convertie en noir et blanc
     return gray, small_frame
 if __name__== "__main__":
     face_locations = []
@@ -33,11 +33,13 @@ if __name__== "__main__":
     print("Preparation avant la video")
     face_cascade = cv2.CascadeClassifier("lbpcascade_frontalface_improved.xml")
     cap = cv2.VideoCapture(0)
-    # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-    # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    cap.set(cv2.CAP_PROP_BUFFERSIZE, 0)
     i=0
     print("Debut")
     while True:
+        i+=1
         _, img = cap.read()
         gray, img = traitement_image(img)
         faces = face_cascade.detectMultiScale(gray, 1.1, 4)#on detecte tous les visages
@@ -48,10 +50,14 @@ if __name__== "__main__":
         # )#on fais un rectangle autour de leur visages
         
         if not isinstance(faces, tuple):#si il y a un visage de detecté (pas un tuple)
-            rgb_small_frame = img[:, :, ::-1]#on converti pour avoir du rgb a la place du bgr (car utilisé par le module)
-
-            face_locations = face_recognition.face_locations(rgb_small_frame)
-            
+            rgb_small_frame = img[:, :, ::-1]#on converti pour avoir du 
+            # rgb a la place du bgr (car utilisé par le module)
+            print(f"faces {faces}")
+            print(face_recognition.face_locations(rgb_small_frame))
+            # face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_locations = faces
+            # print(face_locations)
+            # print(faces)
             face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
             
             for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
@@ -60,10 +66,15 @@ if __name__== "__main__":
                 
                 name = "Unknown"
                 
+                if True in matches:
+                    first_match_index = matches.index(True)
+                    name = known_face_names[first_match_index]
+                    print(f"if true {name}")
                 face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
                 best_match_index = np.argmin(face_distances)
                 if matches[best_match_index]:
                     name = known_face_names[best_match_index]
+                    print(f"imatches{name}")
                 # top*=4
                 # bottom*=4
                 # left*=4
@@ -75,7 +86,8 @@ if __name__== "__main__":
                 # cv2.rectangle(img, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
                 # font = cv2.FONT_HERSHEY_DUPLEX
                 # cv2.putText(img, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-                # cv2.imwrite(f"debug/{i}.png", img)
+                cv2.imwrite(f"debug/rgb.png", rgb_small_frame)
+                cv2.imwrite(f"debug/img.png", img)
         # else:
         #     #cv2.imwrite(f"debug/{i}.png", img)
         #     pass
--- facial_programme/option3.py
+++ facial_programme/option3.py
@@ -18,8 +18,6 @@ known_face_encodings , known_face_names = encodage_visage()
 # Initialize some variables
 face_locations = []
 face_encodings = []
-face_names = []
-process_this_frame = True
 print("debut")
 while True:
     # Grab a single frame of video
@@ -32,31 +30,33 @@ while True:
     rgb_small_frame = small_frame[:, :, ::-1]
 
     # Only process every other frame of video to save time
-    if process_this_frame:
         # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+    print(face_locations)
+    if face_locations != []:
+        print(face_locations)
+    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-        face_names = []
-        for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-            name = "Unknown"
+    face_names = []
+    for face_encoding in face_encodings:
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        name = "Unknown"
 
-            # # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
+        # # If a match was found in known_face_encodings, just use the first one.
+        if True in matches:
+            first_match_index = matches.index(True)
+            name = known_face_names[first_match_index]
+            # print(f"known {name}")
 
-            # Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
-            print(name)
-            face_names.append(name)
+        # Or instead, use the known face with the smallest distance to the new face
+        # face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        # best_match_index = np.argmin(face_distances)
+        # if matches[best_match_index]:
+        #     name = known_face_names[best_match_index]
+        #     print(f"matches {name}")
+        print(name)
 
-    process_this_frame = not process_this_frame
 
     """
     # Display the results
